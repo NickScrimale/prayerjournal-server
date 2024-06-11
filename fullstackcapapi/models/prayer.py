88d77c0e29419c64086c5e401519ceb912b84d6d
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2024-06-11 09:02:30 -0500

    fixed linter errors

--- fullstackcapapi/models/prayer.py
+++ fullstackcapapi/models/prayer.py
@@ -1,6 +1,5 @@
 from django.db import models
 from .user import User
-from datetime import datetime
 
 class Prayer(models.Model):
     uid = models.ForeignKey(User, on_delete=models.CASCADE)
--- fullstackcapapi/models/user.py
+++ fullstackcapapi/models/user.py
@@ -1,7 +1,8 @@
 from django.db  import models
 
 class User(models.Model):
-  uid = models.CharField(max_length=50)
-  first_name = models.CharField(max_length=25)
-  last_name = models.CharField(max_length=25)
-  profile_image_url = models.CharField(max_length=200)
+    '''Class representing User'''
+    uid = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    profile_image_url = models.CharField(max_length=200)
